e7aec0fac4c251ea0590d5317a743be4e1803eea
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-06 15:26:30 +0000

    statusbar added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Text here</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }

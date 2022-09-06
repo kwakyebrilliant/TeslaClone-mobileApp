288f0ba6121f027aa240e7e5ed203f825313bf4a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-06 16:03:23 +0000

    updates

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
-        <View>
-          <Text>Model S</Text>
-          <Text>Starting at $56,764</Text>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $56,764</Text>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -24,5 +24,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
   },
 });

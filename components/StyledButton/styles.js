1bbf4c03a1ab089261837be7e6310dc9373fafd0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 09:45:33 +0000

    styled button updates

--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -1,7 +1,19 @@
 import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
-   
+   container: {
+    width: '100%',
+    padding: 10,
+   },
+   button: {
+    backgroundColor: 'white',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+   },
+   text: {
+
+   },
 });
 
 export default styles;

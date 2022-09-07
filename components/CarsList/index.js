0c063d1c1034c6f6bd8c85d11536ae496eb6381a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 16:29:00 +0000

    carlist updates

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -10,7 +10,11 @@ const CarsList = (props) => {
     <View style={styles.container}>
       <FlatList
         data={cars}
-        renderItem={({item}) => <CarItem car={item}/>} 
+        renderItem={({item}) => <CarItem car={item}/>}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height} 
       />
     </View>
   );

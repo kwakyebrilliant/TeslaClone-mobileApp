0a7d9dbfab20ae4cd8d4d004b00536347d6d59ee
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 16:04:33 +0000

    app updates

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {StatusBar} from 'react-native';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (

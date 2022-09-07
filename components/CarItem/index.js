a40d39c0abcdaa5c30a7a2a8723490ece3840886
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 12:30:37 +0000

    car item updates

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,7 +4,7 @@ import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
-  const {name, tagline, image} = props;
+  const {name, tagline, taglineCTA, image} = props;
   return (
     <View style={styles.carContainer}>
       <ImageBackground 
@@ -13,7 +13,7 @@ const CarItem = (props) => {
       />
         <View style={styles.titles}>
           <Text style={styles.title}>{name}</Text>
-          <Text style={styles.subtitle}>{tagline}</Text>
+          <Text style={styles.subtitle}>{tagline}{' '}<Text style={styles.subtitleCTA}>{taglineCTA}</Text></Text>
         </View>
         <View style={styles.buttonsContainer}>
         <StyledButton 

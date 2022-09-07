01e612e238f8dfe4bc1228bd4d18a6aa2f6755cc
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 16:06:51 +0000

    app updates

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import {StatusBar} from 'react-native';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import CarItem from './components/CarItem';
 import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem 
-        name={'Model 3'} 
-        tagline={'Order online for'}
-        taglineCTA={'Touchless Delivery'}
-        image={require('./assets/images/Model3.jpeg')} 
-        />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );

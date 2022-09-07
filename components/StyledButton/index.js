e7163376d2e02d14ae95ed673f0661c2ddbbbf6f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 10:03:04 +0000

    styled button updates

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -5,10 +5,11 @@ import styles from './styles';
 const StyledButton = (props) => {
     const type = props.type;
     console.warn(type);
+    const backgroundColor = type === 'primary' ? 'black' : 'white';
   return (
     <View style={styles.container}>
       <Pressable
-        style={styles.button}
+        style={[styles.button, {backgroundColor: backgroundColor}]}
         onPress={() => {
         console.warn('Hey there')
         }}

fc82543e01e248e3c4a0e147c6f49868c5fa87f1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 15:55:09 +0000

    carlist updates

---
+++ components/CarsList/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+const CarsList = (props) => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+export default CarsList;
